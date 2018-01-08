30fa0721180fcce230e02bb53ac7571b793a0658
Philippe Lawson <philippe.lawson@lawson-it.com>
2018-01-08 18:58:05 +0100

    Added Remaining Bolts

--- src/main/java/analytics/App.java
+++ src/main/java/analytics/App.java
@@ -11,28 +11,30 @@ import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseWindowedBolt;
 import org.apache.storm.tuple.Fields;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args ) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException
     {
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("page-visits", new PageVisitSpout());
+        builder.setBolt("visit-counts", new VisitCountBolt(), 1)
+                .shuffleGrouping("page-visits");
+        builder.setBolt("user-visit-counts", new UserVisitCountBolt(), 2)
+                .fieldsGrouping("visit-counts", new Fields("userId"));
+        builder.setBolt("page-visit-counts", new PageVisitCountBolt(), 2)
+                .fieldsGrouping("visit-counts", new Fields("url"));
         builder.setBolt("user-page-visit-counts", new UserPageVisitCount()
-                .withWindow(BaseWindowedBolt.Duration.of(1000*60*60), BaseWindowedBolt.Duration.of(1000*30)))
+                .withWindow(BaseWindowedBolt.Duration.of(1000*60*60), BaseWindowedBolt.Duration.of(1000*30)), 2)
                 .fieldsGrouping("page-visits", new Fields("url"));
-
         StormTopology topology = builder.createTopology();
-        Config config = new Config();
-        config.setMessageTimeoutSecs((60*60) + 40);
-        String topologyName = "analytics-topology";
 
-        if (args.length > 1 && args[0].equals("remote")) {
+        Config config = new Config();
+        config.setMessageTimeoutSecs((60*60) + 40); // Message timeout > window length + window sliding duration
+        String topologyName = "analytics-topology";
+        if(args.length > 0 && args[0].equals("remote")) {
             StormSubmitter.submitTopology(topologyName, config, topology);
-        } else {
+        }
+        else {
             LocalCluster cluster = new LocalCluster();
             cluster.submitTopology(topologyName, config, topology);
         }
